e50a2e02c06519b085f33550407e2bebe6c2a63e
Miguel-124 <michalgorecki124@gmail.com>
2023-07-20 18:20:55 +0200

    adding class

---
+++ ProjektSemestralny2/Edit.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using ProjektSemestralny.Database;
+
+namespace ProjektSemestralny
+{
+    /// <summary>
+    /// Interaction logic for Edit.xaml
+    /// </summary>
+    public partial class Edit : Page
+    {
+        public Edit()
+        {
+            InitializeComponent();
+            InputCarName.Text = "";
+            InputMileage.Text = "";
+            InputYear.Text = "";
+            List<Engines> data1 = LoadEngines();
+            SelectEngine.DisplayMemberPath = "Name";
+            SelectEngine.SelectedValuePath = "Id";
+            SelectEngine.DataContext = data1;
+            List<Colors> data2 = LoadColors();
+            SelectColor.DisplayMemberPath = "Name";
+            SelectColor.SelectedValuePath = "Id";
+            SelectColor.DataContext = data2;
+            List<States> data3 = LoadStates();
+            SelectState.DisplayMemberPath = "Name";
+            SelectState.SelectedValuePath = "Id";
+            SelectState.DataContext = data3;
+            List<Id> data4 = LoadIds();
+            SelectId.DisplayMemberPath = "Name";
+            SelectId.SelectedValuePath = "ID";
+            SelectId.DataContext = data4;
+        }
+
+        private List<Engines> LoadEngines()
+        {
+            using (MotoryzacjaDBContext context = new MotoryzacjaDBContext())
+            {
+                List<Engines> engines = context.Engines.ToList();
+                return engines;
+            }
+        }
+
+        private List<Colors> LoadColors()
+        {
+            using (MotoryzacjaDBContext context = new MotoryzacjaDBContext())
+            {
+                List<Colors> colors = context.Colors.ToList();
+                return colors;
+            }
+
+        }
+
+        private List<States> LoadStates()
+        {
+            using (MotoryzacjaDBContext context = new MotoryzacjaDBContext())
+            {
+                List<States> states = context.States.ToList();
+                return states;
+            }
+        }
+
+        private List<Id> LoadIds()
+        {
+            using (MotoryzacjaDBContext context = new MotoryzacjaDBContext())
+            {
+                List<Id> ids = new List<Id>();
+                List<Cars> allCars = context.Cars.ToList();
+                foreach (Cars car in allCars){
+                    Id currentId = new Id()
+                    {
+                        ID = car.Id,
+                        Name = car.Id.ToString()
+                    };
+                    ids.Add(currentId);
+                }
+                return ids;
+            }
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void btnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectId.SelectedValue != null && InputCarName.Text != "" && InputMileage.Text != "" && InputYear.Text != "" && SelectEngine.SelectedValue != null && SelectColor.SelectedValue != null && SelectState.SelectedValue != null)
+            {
+                using (MotoryzacjaDBContext context = new MotoryzacjaDBContext())
+                {
+                    int properId = Int32.Parse(SelectId.SelectedValue.ToString());
+                    Cars toEdit = (from car in context.Cars
+                                 where car.Id == properId
+                                 select car).First();
+
+                    toEdit.CarName = InputCarName.Text;
+                    toEdit.Engine = Int32.Parse(SelectEngine.SelectedValue.ToString());
+                    toEdit.Color = Int32.Parse(SelectColor.SelectedValue.ToString());
+                    toEdit.State = Int32.Parse(SelectState.SelectedValue.ToString());
+                    toEdit.CarMileage = Int32.Parse(InputMileage.Text);
+                    toEdit.Year = Int32.Parse(InputYear.Text);
+                    context.SaveChanges();
+                }
+                this.NavigationService.Navigate((new List()));
+            }
+            else
+            {
+                labelError.Content = "Musisz uzupełnić wszystkie pola";
+            }
+        }
+    }
+}
